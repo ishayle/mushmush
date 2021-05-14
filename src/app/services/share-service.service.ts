a3236876c48525325d27d9ef7ff7ef59797a1cdf
ishayle <ishayle@gmail.com>
2021-05-14 17:57:23 +0300

    3 files modified, 1 deleted

--- src/app/services/share-service.service.ts
+++ src/app/services/share-service.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Order } from '../model/order';
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
-
 @Injectable()
 export class ShareService {
   constructor() {}
 
   shareOrder(order: Order) {
-    const doc = new jsPDF();
-    doc.autoTable(
-      ['פריט', 'כמות', 'ספק'],
-      order.items.map(i => [i.name, i.amount, i.dealer])
-    );
-    doc.save('table.pdf');
+    // const doc = new jsPDF();
+    // doc.addFileToVFS('IBMPlexSans-Bold.ttf', fonts.base64font);
+    // doc.addFont('IBMPlexSans-Bold.ttf', 'PlexBold', 'normal');
+    // doc.setFont('PlexBold');
+    // doc.autoTable(
+    //   ['פריט', 'כמות', 'ספק'],
+    //   order.items.map(i => [i.name, i.amount, i.dealer])
+    // );
+    // doc.save('table.pdf');
   }
 }
