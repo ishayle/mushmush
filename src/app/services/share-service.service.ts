2e7ebc58b571fc58c1667430af09a006a193a953
ishayle <ishayle@gmail.com>
2021-05-15 16:30:46 +0300

    2 files modified

--- src/app/services/share-service.service.ts
+++ src/app/services/share-service.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 import { Order } from '../model/order';
 import { fonts } from '../fonts';
 import jsPDF from 'jspdf';
-
-//import pdfmake from 'pdfmake/build/pdfmake';
-//import pdfFonts from 'pdfmake/build/vfs_fonts';
+import autoTable from 'jspdf-autotable';
 
 @Injectable()
 export class ShareService {
@@ -15,15 +13,12 @@ export class ShareService {
     doc.addFileToVFS('MyFont.ttf', fonts.alef);
     doc.addFont('MyFont.ttf', 'MyFont', 'normal');
     doc.setFont('MyFont');
-    // doc.addFileToVFS('IBMPlexSans-Bold.ttf', fonts.base64font);
-    // doc.addFont('IBMPlexSans-Bold.ttf', 'PlexBold', 'normal');
-    // doc.setFont('PlexBold');
-    // doc.autoTable(
-    //   ['פריט', 'כמות', 'ספק'],
-    //   order.items.map(i => [i.name, i.amount, i.dealer])
-    // );
-    doc.text(50, 50, 'Now asdשדגכשדגכ is Calibri');
-
+    doc.setR2L(true);
+    autoTable(doc, {
+      head: [['פריט', 'כמות', 'ספק']],
+      body: order.items.map(i => [i.name, i.amount, i.dealer]),
+      styles: { font: 'MyFont' }
+    });
     doc.save('table.pdf');
   }
 }
